6e188643026ed659656323b7758dbab4e61b4827
a.skorik <a.skorik@petrovich.ru>
2023-09-18 21:00:04 +0300

    edited test_python_8_2.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selene.support.shared import browser
 
+#  url открываемого сайтк
+URL = 'https://google.com'
+
 
 @pytest.fixture(scope='function')
 def open_browser_full_screen():
     browser.driver.fullscreen_window()
-    browser.open('https://google.com')
+    browser.open(URL)
     yield browser
     browser.quit()
