9bc95d594338cb189f9291a9c14de8a341c6fe44
a.skorik <a.skorik@petrovich.ru>
2023-09-19 09:18:39 +0300

    edited conftest.py, test_python_8_2.py

--- test/conftest.py
+++ test/conftest.py
@@ -6,8 +6,13 @@ URL = 'https://google.com'
 
 
 @pytest.fixture(scope='function')
-def open_browser_full_screen():
-    browser.driver.fullscreen_window()
+def browser_setup():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+
+@pytest.fixture(scope='function')
+def open_url(browser_setup):
     browser.open(URL)
     yield browser
     browser.quit()
--- test/test_python_8_2.py
+++ test/test_python_8_2.py
@@ -1,14 +1,17 @@
 """Домашнее задание урок - 2. Погружаемся в инструментарий и библиотеки."""
+import pytest
 from selene import be, have
 
 
 # Тест на удачный поиск в Google
-def test_successful_search(open_browser_full_screen):
-    open_browser_full_screen.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    open_browser_full_screen.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
+def test_successful_search(open_url):
+    open_url.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    open_url.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
 
 
 # Тест на пустой поиск в Google
-def test_empty_search(open_browser_full_screen):
-    open_browser_full_screen.element('[name="q"]').should(be.blank).type('hjhsdgfukjsdhf;lasdfp[asodfojusdoifghslkd]').press_enter()
-    open_browser_full_screen.element('#extabar #result-stats').should(have.text('Результатов: примерно 0'))
+@pytest.mark.parametrize("find_string", ['hjhsdgfukjsdhf;lasdfp[asodfojusdoifghslkd]'])
+def test_empty_search(open_url, find_string):
+    open_url.element('[name="q"]').should(be.blank).type(find_string).press_enter()
+    open_url.element('#extabar #result-stats').should(have.text('Результатов: примерно 0'))
+    print(f'При поиске строки - {find_string}, результатов не обнаружено')
